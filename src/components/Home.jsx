8349af8a1937f0b3d3493e69aff691a26bc91632
Raúl <raul.rodriguez.c.2012@gmail.com>
2024-11-23 17:51:41 -0300

    componentes Home y Pizza

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,33 +1,37 @@
-import { Header } from "./Header";
+import { useState,useEffect } from "react";
 import CardPizza from "./CardPizza";
-import { pizzas } from "../assets/JS/pizzas";
+
 
 const Home = () => {
-    console.log(pizzas);
 
+    const [pizzas, setPizzas] = useState([])
+    const backEnd= "http://localhost:5000/api/pizzas"
+  
+    useEffect(() => {
+      const consultaApi = async () => {
+        const res = await fetch(backEnd)
+        const data= await res.json()
+        setPizzas(data) 
+      }
+      consultaApi()
+    }, [])
     return (
-        <>
-        <Header/>
-        <div className="container mt-5 pizzacont">
-            <div className="row">
-        {pizzas.map((pizza) => (
-            <div className="col-md-4" key={pizza.id}>
-            <CardPizza
-            key={pizza.id}
-            name={pizza.name}
-            desc={pizza.desc}
-            img={pizza.img}
-            ingredients={pizza.ingredients}
-            price={pizza.price}
-            />   
-            </div>
+      <>
+   <div className="pizzaContainer">
+      {pizzas.map(
+        (pizza) => (
+          <CardPizza
+          key={pizza.id}
+          name={pizza.name}
+          img={pizza.img}
+          ingredients={pizza.ingredients}
+          price={pizza.price}
+          />
         )
-    )}
-    </div>
-    </div>
-        </>
-    
-    );
-};
+      )}</div>
+      </>
+    )
+  }
+  
+  export default Home
 
-export default Home;
--- src/components/Pizza.jsx
+++ src/components/Pizza.jsx
@@ -1,13 +1,37 @@
 import { useEffect, useState } from "react"
+import CardPizza from "./CardPizza"
 
-useEffect
+
 
 const Pizza = () => {
 
   const [pizzas, setPizzas] = useState([])
-  const backEnd= "../assets/simple-api-backend-nodejs-express-fs-json-jwt-main/index.js"
+  const backEnd= "http://localhost:5000/api/pizzas/p001"
+
+  useEffect(() => {
+    const consultaApi = async () => {
+      const res = await fetch(backEnd)
+      const data= await res.json()
+      setPizzas(data) 
+    }
+    consultaApi()
+  }, [])
   return (
-    <div>Pizza</div>
+    <>
+ <div className="pizzaContainer">
+    {pizzas.map(
+      (pizza) => (
+        <CardPizza
+        key={pizza.id}
+        name={pizza.name}
+        desc={pizza.desc}
+        img={pizza.img}
+        ingredients={pizza.ingredients}
+        price={pizza.price}
+        />
+      )
+    )}</div>
+    </>
   )
 }
 
